1e4da7a7b84ecc5f7400028c292e1f3064d339d8
nickyhuyskens <nicky.huyskens@me.com>
2024-08-15 09:45:42 +0200

    Update custom_config.h only num flip

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,3 +2,9 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_CLIPBOARD_MAC
+
+#define MIRYOKU_ALTERNATIVES_NUM \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp EQUAL,         &kp N4,            &kp N5,            &kp N6,            &kp SEMI,          \
+U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            &kp GRAVE,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp N0,            &kp DOT,           U_NP,              U_NP
